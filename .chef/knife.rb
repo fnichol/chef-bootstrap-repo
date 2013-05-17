a972f72857acc8d8c52b82f10620ea6624001e27
Fletcher Nichol <fnichol@nichol.ca>
2013-05-17 09:15:22 -0600

    Major updates to knife.rb and bootstrap script.

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,66 +1,34 @@
-### Chef Solo
-#
-#
-### Opscode Hosted Chef Server
-#
-#     export KNIFE_USER="jdoe"
-#     export KNIFE_ORGNAME="acmeco"
-#
-# * Your Opscode client key should be at `~/.chef.d/opscode-jdoe.pem`.
-# * Your Opscode validation key should be at `~/.chef.d/opscode-acmeco-validator.pem`.
-#
 ### Chef Server
 #
-#    export KNIFE_USER="hsolo"
-#    export KNIFE_SERVER_NAME="widgetinc"
-#    export KNIFE_SERVER_URL="https://chef.widgetinc.com"
+# KNIFE_CHEF_SERVER             - the URL for the Chef Server
+# KNIFE_VALIDATION_CLIENT_NAME  - validation client name, default is
+#                                 'chef-validator'
 #
-# * Your Chef Server client key should be at `~/.chef.d/widgetinc-hsolo.pem`.
-# * Your Chef Server validation key should be at `~/.chef.d/widgetinc-validator.pem`.
-#
+chef_dir = "#{ENV['HOME'] || ENV['HOMEDRIVE']}/.chef"
 
-current_dir = File.dirname(__FILE__)
-home_dir    = ENV['HOME']
-chef_dir    = "#{home_dir}/.chef"
-chefd_dir   = "#{home_dir}/.chef.d"
+# chef server location and validation key
+chef_server_url           ENV['KNIFE_CHEF_SERVER_URL']
+validation_client_name    ENV['KNIFE_VALIDATION_CLIENT_NAME'] ||
+                          'chef-validator'
+validation_key            ENV['KNIFE_VALIDATION_KEY'] ||
+                          "#{chef_dir}/validation.pem"
 
-user        = ENV['KNIFE_USER'] || ENV['USER']
-orgname     = ENV['KNIFE_ORGNAME']
-server_name = ENV['KNIFE_SERVER_NAME']
-server_url  = ENV['KNIFE_SERVER_URL']
+# user/client name and key
+node_name                 (ENV['KNIFE_USER'] || ENV['USER'] ||
+                           ENV['USERNAME']).downcase
+client_key                ENV['KNIFE_CLIENT_KEY'] ||
+                          "#{chef_dir}/#{node_name}.pem"
 
 # path to cookbooks
-cookbook_path             ["#{current_dir}/../cookbooks",
-                          "#{current_dir}/../site-cookbooks"]
+cookbook_path             ["#{File.dirname(__FILE__)}/../cookbooks"]
 
 # logging details
 log_level                 :info
 log_location              STDOUT
 
-# user/client and private key to authenticate to a Chef Server, if needed
-node_name                 user
-
-if orgname
-  # if KNIFE_ORGNAME is given, then we're talking to the Opscode Hosted Chef
-  # Server
-  validation_client_name  "#{orgname}-validator"
-  client_key              "#{chefd_dir}/opscode-#{user}.pem"
-  validation_key          "#{chefd_dir}/opscode-#{orgname}-validator.pem"
-  chef_server_url         "https://api.opscode.com/organizations/#{orgname}"
-elsif server_name
-  # if KNIFE_SERVER_NAME is defined, then we're talking to a Chef Server
-  validation_client_name  "chef-validator"
-  client_key              "#{chefd_dir}/#{server_name}-#{user}.pem"
-  validation_key          "#{chefd_dir}/#{server_name}-validator.pem"
-  chef_server_url         server_url
-end
-
-# set default chef environment when bootstrapping
-environment               "stable"
-
 # caching options
 cache_type                'BasicFile'
-cache_options( :path =>   "#{home_dir}/.chef/checksums" )
+cache_options( :path =>   "#{chef_dir}/checksums" )
 
 file_backup_path          "#{chef_dir}/backups"
 
@@ -69,13 +37,7 @@ cookbook_copyright        ENV['KNIFE_COOKBOOK_COPYRIGHT'] ||
                           %x{git config --get user.name}.chomp
 cookbook_email            ENV['KNIFE_COOKBOOK_EMAIL'] ||
                           %x{git config --get user.email}.chomp
-cookbook_license          "apachev2"
-
-# rackspace configuration
-if ENV['RACKSPACE_USERNAME'] && ENV['RACKSPACE_API_KEY']
-  knife[:rackspace_api_username]  = ENV['RACKSPACE_USERNAME']
-  knife[:rackspace_api_key]       = ENV['RACKSPACE_API_KEY']
-end
+cookbook_license          'apachev2'
 
 # aws ec2 configuration
 if ENV['AWS_AWS_ACCESS_KEY_ID'] && ENV['AWS_AWS_SECRET_ACCESS_KEY']
@@ -83,12 +45,12 @@ if ENV['AWS_AWS_ACCESS_KEY_ID'] && ENV['AWS_AWS_SECRET_ACCESS_KEY']
   # Searches the ENV hash for keys starting with "AWS_" and converts them
   # to knife config settings. For example:
   #
-  #     ENV['AWS_AWS_ACCESS_KEY_ID'] = "abcabc"
+  #     ENV['AWS_ACCESS_KEY_ID'] = "abcabc"
   #     ENV['AWS_FLAVOR'] = "t1.small"
   #
   # becomes:
   #
-  #     knife[:aws_access_key_id] = "abcabc"
+  #     knife[:access_key_id] = "abcabc"
   #     knife[:flavor] = "t1.small"
   aws_attrs = ENV.keys.select { |k| k =~ /^AWS_/ }
 
@@ -97,13 +59,19 @@ if ENV['AWS_AWS_ACCESS_KEY_ID'] && ENV['AWS_AWS_SECRET_ACCESS_KEY']
   end
 end
 
-# linode configuration
-if ENV['LINODE_API_KEY']
-  knife[:linode_api_key] = ENV['LINODE_API_KEY']
-end
-
 # bluebox configuration
 if ENV['BLUEBOX_CUSTOMER_ID'] && ENV['BLUEBOX_API_KEY']
   knife[:bluebox_customer_id] = ENV['BLUEBOX_CUSTOMER_ID']
   knife[:bluebox_api_key]     = ENV['BLUEBOX_API_KEY']
+end
+
+# rackspace configuration
+if ENV['RACKSPACE_USERNAME'] && ENV['RACKSPACE_API_KEY']
+  knife[:rackspace_api_username]  = ENV['RACKSPACE_USERNAME']
+  knife[:rackspace_api_key]       = ENV['RACKSPACE_API_KEY']
+end
+
+# allow overriding values in this knife.rb
+if ENV['KNIFE_OVERRIDE'] && File.exist?(ENV['KNIFE_OVERRIDE'])
+  Chef::Config.from_file(ENV['KNIFE_OVERRIDE'])
 end
